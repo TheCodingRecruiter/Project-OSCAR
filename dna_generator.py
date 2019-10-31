68267405d97efe1e8453a343e4238758b0adeca8
Richard Eby <richard.eby@hotmail.com>
2019-10-31 10:47:34 -0400

    Dna Generator
    Need to add in the ability to create a list of these DNA strings

---
+++ dna_generator.py
@@ -0,0 +1,30 @@
+# 3 billion base pairs
+# A, G, T, C
+
+import random
+
+def complet_random():
+    a = random.randint(1,100)
+    # print(a)
+    for i in range(a):
+        b = random.randint(1,100)
+        # print(b)
+        for i in range(b):
+            c = random.randint(1,100)
+            # print(c)
+    return c
+dna_pairs = []
+def random_list_choice(list):
+    a = complet_random()
+    for i in range(a):
+        x = random.choice(list)
+    dna_pairs.append(x)
+base_pairs = ['A', 'G', 'T', 'C']
+how_many_pairs = 300000000
+for i in range(how_many_pairs):
+    random_list_choice(base_pairs)
+x = ''
+for i in dna_pairs:
+    x = str(x + i)
+
+print(f'This DNA Strand has {how_many_pairs} pairs: {x}')
